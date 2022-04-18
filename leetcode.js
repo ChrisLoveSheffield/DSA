b10b9820693a1ad44d638fa16b20dcdcdffb6119
Chunyuan Huang <chris.huang1@aecom.com>
2022-04-18 20:50:37 +0800

    set

--- leetcode.js
+++ leetcode.js
@@ -4,28 +4,27 @@
  * @return {number}
  */
 var maxCoins = function (nums) {
-    const dp = new Array(nums.length).fill().map(() => new Array(nums.length).fill())
-    /**
-     *
-     * @param {number[]} nums
-     */
-    const maxCoinsHelper = (left, right) => {
-        if (left > right) return 0
+  const dp = new Array(nums.length)
+    .fill()
+    .map(() => new Array(nums.length).fill());
 
-        if (dp[left][right]) return dp[left][right]
+  const maxCoinsHelper = (left, right) => {
+    if (left > right) return 0;
 
-        let max = 0
-        for (let i = left; i <= right; i++) {
-            let temp =
-                maxCoinsHelper(left, i - 1) +
-                maxCoinsHelper(i + 1, right) +
-                (nums[i - 1] ?? 1) * nums[i] * (nums[i + 1] ?? 1)
-            max = Math.max(temp, max)
-        }
-        dp[left][right] = max
-        return max
+    if (dp[left][right]) return dp[left][right];
+
+    let max = 0;
+    for (let i = left; i <= right; i++) {
+      let temp =
+        maxCoinsHelper(left, i - 1) +
+        maxCoinsHelper(i + 1, right) +
+        (nums[left - 1] ?? 1) * nums[i] * (nums[right + 1] ?? 1);
+      max = Math.max(temp, max);
     }
-    return maxCoinsHelper(0, nums.length - 1)
-}
-let nums = [3, 1, 5, 8]
-console.log(maxCoins(nums))
+    dp[left][right] = max;
+    return max;
+  };
+  return maxCoinsHelper(0, nums.length - 1);
+};
+let nums = [3, 1, 5, 8];
+console.log(maxCoins(nums));
