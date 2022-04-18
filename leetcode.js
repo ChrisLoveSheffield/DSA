9a181f821ded28eca6b500bcf6c266ecb8ecf59f
Chunyuan Huang <chris.huang1@aecom.com>
2022-04-18 09:43:25 +0800

    git

--- leetcode.js
+++ leetcode.js
@@ -1,7 +1,31 @@
+// @flow
 /**
- * @param {number[][]} pairs
+ * @param {number[]} nums
  * @return {number}
  */
- var findLongestChain = function(pairs) {
-    
-};
+var maxCoins = function (nums) {
+    const dp = new Array(nums.length).fill().map(() => new Array(nums.length).fill())
+    /**
+     *
+     * @param {number[]} nums
+     */
+    const maxCoinsHelper = (left, right) => {
+        if (left > right) return 0
+
+        if (dp[left][right]) return dp[left][right]
+
+        let max = 0
+        for (let i = left; i <= right; i++) {
+            let temp =
+                maxCoinsHelper(left, i - 1) +
+                maxCoinsHelper(i + 1, right) +
+                (nums[i - 1] ?? 1) * nums[i] * (nums[i + 1] ?? 1)
+            max = Math.max(temp, max)
+        }
+        dp[left][right] = max
+        return max
+    }
+    return maxCoinsHelper(0, nums.length - 1)
+}
+let nums = [3, 1, 5, 8]
+console.log(maxCoins(nums))
